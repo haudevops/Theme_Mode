8111cb013f59f96bcc799af1e912d4a729e6c2fe
Hau Tran <hau.tran@eton.vn>
2021-10-28 10:01:05 +0700

    Update 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_learning/provider/theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
+import 'until/constants.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  runApp(ChangeNotifierProvider(
+      create: (_) => ProviderThemeMode(isDarkMode: prefs.getBool(Constants.ThemeMode)),
+      child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => ProviderThemeMode(),
-      child: Consumer<ProviderThemeMode>(
-        builder: (context, providerTheme, child){
-          return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              theme: providerTheme.isDark ? dark : light,
-              home: MyHomePage()
-          );
-        },
-      ),
+    return Consumer<ProviderThemeMode>(
+      builder: (context, providerTheme, child) {
+        return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: providerTheme.getTheme,
+            home: MyHomePage());
+      },
     );
   }
 }
@@ -38,11 +41,14 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('Theme Mode'),
         actions: [
-          Switch(
-              value: context.watch<ProviderThemeMode>().isDark,
-              onChanged: (newValue) {
-                Provider.of<ProviderThemeMode>(context, listen: false).setTheme(newValue);
-              }),
+          IconButton(
+            icon: Icon(Icons.brightness_6),
+            color: Colors.white,
+            onPressed: () {
+              Provider.of<ProviderThemeMode>(context, listen: false)
+                  .checkTheme();
+            },
+          ),
         ],
       ),
       body: Column(
--- lib/provider/theme.dart
+++ lib/provider/theme.dart
@@ -3,6 +3,8 @@ import 'package:flutter/scheduler.dart';
 import 'package:provider_learning/until/constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../until/constants.dart';
+
 ThemeData light = ThemeData(
     brightness: Brightness.light,
 );
@@ -13,29 +15,32 @@ ThemeData dark = ThemeData(
 
 class ProviderThemeMode extends ChangeNotifier{
   Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
-  bool _isDark;
+  ThemeData _selectTheme;
 
-  bool get isDark => _isDark;
+  ThemeData light = ThemeData(
+    brightness: Brightness.light,
+  );
 
-  ProviderThemeMode(){
-    _isDark = false;
-    checkTheme();
+  ThemeData dark = ThemeData(
+    brightness: Brightness.dark,
+  );
+
+  ProviderThemeMode({bool isDarkMode}){
+    _selectTheme = isDarkMode ? dark : light;
   }
 
-  void setTheme(value) async{
+  void checkTheme() async {
     SharedPreferences prefs = await _prefs;
-    prefs.setBool(Constants.ThemeMode, value);
-    _isDark = value;
+    if (_selectTheme == dark) {
+      _selectTheme = light;
+      await prefs.setBool(Constants.ThemeMode, false);
+    } else {
+      _selectTheme = dark;
+      await prefs.setBool(Constants.ThemeMode, true);
+    }
+
     notifyListeners();
   }
 
-  void checkTheme() async{
-    SharedPreferences prefs = await _prefs;
-    var brightness = SchedulerBinding.instance.window.platformBrightness;
-    if(prefs.getBool(Constants.ThemeMode) != null){
-      setTheme(_isDark);
-    }else{
-      setTheme(brightness);
-    }
-  }
+  ThemeData get getTheme => _selectTheme;
 }
